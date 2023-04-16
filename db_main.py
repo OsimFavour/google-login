4132558e96bb00b8c7e0f4bbb7b26d9a9abec734
Osim Favour <famos204@gmail.com>
2023-04-16 23:26:31 +0100

    Database with Google Oauth

---
+++ db_main.py
@@ -0,0 +1,95 @@
+"""Can I have a simple code to login google authenticated users in my flask website also creating a database for them.
+   Sure, here's a simple example code that uses Flask-Login for user authentication and SQLAlchemy for database 
+   operations. This assumes that you have already created a Google API project and have obtained the client ID and
+   client secret.
+"""
+
+from flask import Flask, redirect, url_for, render_template, request
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
+from flask_sqlalchemy import SQLAlchemy
+from google.oauth2 import id_token
+from google.auth.transport import requests
+
+# Initialize Flask app
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = 'super-secret-key'
+
+# Initialize SQLAlchemy
+db = SQLAlchemy(app)
+
+# Initialize LoginManager
+login_manager = LoginManager(app)
+
+# Initialize Google OAuth settings
+CLIENT_ID = 'your-client-id'
+CLIENT_SECRET = 'your-client-secret'
+REDIRECT_URI = 'http://localhost:5000/google-auth'
+
+# Define User model for database
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    google_id = db.Column(db.String(100), unique=True)
+
+# Load user from User ID
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Login page
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+# Google authentication
+@app.route('/google-auth')
+def google_auth():
+    code = request.args.get('code')
+    token_response = requests.post('https://oauth2.googleapis.com/token', data={
+        'code': code,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'redirect_uri': REDIRECT_URI,
+        'grant_type': 'authorization_code'
+    })
+    token_data = token_response.json()
+    id_token_data = id_token.verify_oauth2_token(token_data['id_token'], requests.Request(), CLIENT_ID)
+    user = User.query.filter_by(google_id=id_token_data['sub']).first()
+    if user is None:
+        user = User(google_id=id_token_data['sub'])
+        db.session.add(user)
+        db.session.commit()
+    login_user(user)
+    return redirect(url_for('index'))
+
+# Logout
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+# Protected page
+@app.route('/')
+@login_required
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
+
+"""
+The above code creates a SQLite database file named users.db in the same directory as the script. 
+It also creates a User table with columns for the user ID and Google ID. When a user logs in with 
+Google authentication, their Google ID is checked against the database. If it is not found, a new 
+user is created and added to the database.
+
+The @login_required decorator is used to protect the index page, which can only be accessed by 
+authenticated users. The @login_manager.user_loader decorator is used to load the user from the User ID.
+
+You will need to create two HTML templates, login.html and index.html, to render the login page and the 
+protected page respectively. You can customize these templates to match your website's design.
+
+I hope this helps you get started with user authentication and database operations in Flask!
+"""
